43716351863be4ba0c2678f42aeec572ee8d4018
Sneha Purkayastha <iamsneha0296@gmail.com>
2022-11-17 14:56:50 +0530

    image and data added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        firstFont: 'Montserrat',
+        secondFont: 'Roboto'
+      },
+      colors:{
+        bodybackground: '#F2F2F2',
+        colorone: '#4F4F4F',
+        colortwo: '#828282',
+        colorthree: '#C4C4C4',
+        colorfour: '#2F80ED'
+      },
+      width:{
+        pWidth: '370px',
+        skillW: '500px'
+      },
+      gap:{
+        first: '85px',
+        second: '78px'
+      }
+    },
   },
   plugins: [],
 }
